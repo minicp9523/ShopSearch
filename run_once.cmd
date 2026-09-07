61709732f42d236f2ad408e431a1145d8af1fcc9
minicp9523 <minicp9523@users.noreply.github.com>
2026-09-07 17:34:37 +0800

    Remove obsolete Playwright implementation

--- run_once.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-".venv\Scripts\python.exe" -u main.py --once
-echo.
-echo Program finished. Press any key to close this window.
-pause >nul
-
--- run_tracker.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-".venv\Scripts\python.exe" -u main.py
-echo.
-echo Program finished. Press any key to close this window.
-pause >nul
